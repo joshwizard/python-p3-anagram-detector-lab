31dea9064107461224f8340ce9c1f87234c48208
joshwizard <murikij@gmail.com>
2025-11-18 17:35:59 +0300

    Added some code

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, word_list):
+        matches = []
+        for word in word_list:
+            if sorted(self.word.lower()) == sorted(word.lower()):
+                matches.append(word)
+        return matches
+  
